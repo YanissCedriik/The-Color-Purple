03b35280c07631f0462df4aa8bb42f8a8bfd52b3
YanissCedriik <167817702+YanissCedriik@users.noreply.github.com>
2024-10-01 19:08:21 +0200

    It works?? Why???

--- JS/view.js
+++ JS/view.js
@@ -13,17 +13,17 @@ function updateView() {
   <div class="dropdown">
       <button class="dropbtn">Browse</button>
       <div class="dropdown-content">
-          <a onclick="filterUserSpecifiedMovies("Action")"    href="#">Action</a>
-          <a onclick="filterUserSpecifiedMovies("Animation")" href="#">Animation</a>
-          <a onclick="filterUserSpecifiedMovies("Adventure")" href="#">Adventure</a>
-          <a onclick="filterUserSpecifiedMovies("Comedy")"    href="#">Comedy</a>
-          <a onclick="filterUserSpecifiedMovies("Crime")"     href="#">Crime</a>
-          <a onclick="filterUserSpecifiedMovies("Drama")"     href="#">Drama</a>
-          <a onclick="filterUserSpecifiedMovies("Sci-Fi")"    href="#">Sci-Fi</a>
-          <a onclick="filterUserSpecifiedMovies("Spy")"       href="#">Spy</a>
-          <a onclick="filterUserSpecifiedMovies("Thriller")"  href="#">Thriller</a>
-          <a onclick="filterUserSpecifiedMovies("Romance")"   href="#">Romance</a>
-          <a onclick="filterUserSpecifiedMovies("War")"       href="#">War</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Action</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Animation</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Adventure</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Comedy</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Crime</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Drama</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Sci-Fi</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Spy</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Thriller</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">Romance</a>
+          <a onclick="filterUserSpecifiedMovies(this.innerHTML)" href="#">War</a>
       </div>
   </div>
   <button>Home</button>
